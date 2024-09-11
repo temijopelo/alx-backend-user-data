7b5e44a9e877edda99513d01024fdf28510ac4ee
temijopelo <temijeje@gmail.com>
2024-09-11 02:40:22 +0100

    update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
+"""Route module for the API.
 """
-Route module for the API
-"""
+import os
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 
@@ -25,9 +24,10 @@ if auth_type == 'basic_auth':
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """Not found handler.
     """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
@@ -60,7 +60,6 @@ def authenticate_user():
                 abort(401)
             if user is None:
                 abort(403)
-
 
 
 if __name__ == "__main__":
